d30bd891274bcafe81cfd74cf420713b4041df12
evecarina <carina120794@gmail.com>
2017-12-03 12:25:25 -0500

    ordenando sesiones, en un bloque cada uno , poniendole barra de navegacion

--- main.js
+++ main.js
@@ -111,23 +111,14 @@ $(document).ready(function(){
   }); 
 }); 
 
-/*
 $(document).ready(function(){
-  $('a[href^="#"]').on('click',function (e){
-    e.preventDefault();
-    var target = this.hash;
-    var $target = $(target);
-    
-    //scroll con hash
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-    }, 1000, 'swing', function(){
-      window.location.hash = target;
-    });
-    
-   /* //scroll sin hash
-    $('html, body').animte({
-      'scrollTop': $targer.offset().top
-      }, 1000, 'swing');
-  });
-});*/
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 100 ){
+			$('nav').addClass('navbar-default2');
+		} else {
+			$('nav').removeClass('navbar-default2');
+		}
+	});
+ 
+});
